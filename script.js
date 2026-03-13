98a8fbf1f2029a24b1a089747fb8fa357b47e4fc
Tracy Lethoko <tracylethoko@Tracys-MacBook-Pro.local>
2026-03-13 19:37:32 +0200

    in script.js file created variables to render them dynamically and add to correctly display according to their status. Also created basic modal that searched clicked selected tak according to it

---
+++ script.js
@@ -0,0 +1,60 @@
+const todoContainer = document.getElementById("todo-tasks");
+const doingContainer = document.getElementById("doing-tasks");
+const doneContainer = document.getElementById("done-tasks");
+
+const modal = document.getElementById("task-modal");
+const inputTitle = document.getElementById("task-title");
+const inputDesc = document.getElementById("task-desc");
+const selectStatus = document.getElementById("task-status");
+
+function renderTasks() {
+
+    console.log(initialTasks);
+
+    initialTasks.forEach(function (task) {
+  
+      const taskElement = document.createElement("div");
+  
+      taskElement.classList.add("task-div");
+      taskElement.textContent = task.title;
+  
+      // store task id in the DOM element
+      taskElement.dataset.id = task.id;
+  
+      taskElement.addEventListener("click", function () {
+        openModal(task.id);
+      });
+  
+      // if statements to iterate through array and place task in correct column
+      if (task.status === "todo") {
+        todoContainer.appendChild(taskElement);
+      }
+  
+      if (task.status === "doing") {
+        doingContainer.appendChild(taskElement);
+      }
+  
+      if (task.status === "done") {
+        doneContainer.appendChild(taskElement);
+      }
+  
+    });
+  
+  }
+
+  function openModal(taskId) {
+
+    const task = initialTasks.find(function (taskItem) {
+        return taskItem.id === taskId;
+      });
+  
+    if (!task) return;
+  
+    inputTitle.value = task.title;
+    inputDesc.value = task.description;
+    selectStatus.value = task.status;
+  
+    modal.showModal();
+
+  }
+  renderTasks();
