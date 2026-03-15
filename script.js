2c0d1ebb221843c98def3ead50a2323b04cace60
Tracy Lethoko <tracylethoko@Tracys-MacBook-Pro.local>
2026-03-15 08:54:02 +0200

    added jsDoc comments to script fuctions and cleaned up code base

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const inputTitle = document.getElementById("task-title");
 const inputDesc = document.getElementById("task-desc");
 const selectStatus = document.getElementById("task-status");
 
+/**
+ * Renders all tasks from the initialTasks array
+ * using task id to add it to the DOM accoring to their status
+ */
 function renderTasks() {
 
     console.log(initialTasks);
@@ -18,14 +22,12 @@ function renderTasks() {
       taskElement.classList.add("task-div");
       taskElement.textContent = task.title;
   
-      // store task id in the DOM element
       taskElement.dataset.id = task.id;
   
       taskElement.addEventListener("click", function () {
         openModal(task.id);
       });
   
-      // if statements to iterate through array and place task in correct column
       if (task.status === "todo") {
         todoContainer.appendChild(taskElement);
       }
@@ -42,6 +44,11 @@ function renderTasks() {
   
   }
 
+  /**
+   * Finds a task by ID and adds it to the modal with 
+   * all it's details before opening it
+   * @param {number} taskId  The identifier for the task to display
+   */
   function openModal(taskId) {
 
     const task = initialTasks.find(function (taskItem) {
@@ -57,8 +64,11 @@ function renderTasks() {
     modal.showModal();
 
   }
+  
   renderTasks();
 
+
+  //listens for when button is clicked so it can close modal
   const closeBtn = document.querySelector(".close-modal");
 
   closeBtn.addEventListener("click", function () {
